d3bf0684da63ea81ae3ec9acb6de3ed5faa7514b
Benjamin Cherelus <105877269+ssigment1997@users.noreply.github.com>
2025-05-25 20:34:40 -0400

    Update Ch13Exercise17.cpp

--- Ch13Exercise17.cpp
+++ Ch13Exercise17.cpp
@@ -2,13 +2,13 @@
 #include <random>
 
 int main() {
-    // Use random device to seed the random number generator
+    
     std::random_device rd;
 
-    // Create a random number generator engine
+    // This is the random number generator engine
     std::mt19937 gen(rd());
 
-    // Define the distribution for real numbers between 10 and 100
+    // distribution for real numbers between 10 and 100
     std::uniform_real_distribution<> dis(10.0, 100.0);
 
     std::cout << "25 random real numbers between 10 and 100:\n";
